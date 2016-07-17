9ec804de304261f9804023a643382e898322495f
= <=>
2016-07-17 17:01:17 +0200

    Multithread image compression

--- ImageServer.py
+++ ImageServer.py
@@ -3,6 +3,13 @@ from Camera import Camera
 import threading, cv2
 
 class ImageServer:
+	def compressThread(self):
+		while self.serve == True:
+			while len(self.compressQueue) == 0: time.sleep(0.05) #Wait for an image to appear
+			sliceNr, img = self.compressQueue.pop(0)
+			ret, img = cv2.imencode(".jpg", img, [cv2.IMWRITE_JPEG_QUALITY, 95])
+			self.sendQueue.append((sliceNr, img)) #Compress the image and make it ready for sending
+		
 	def imageServerThread(self, captureWidth, captureHeight, buffersize, bindPort=5010, bindIP="0.0.0.0"):
 		cam = Camera(captureWidth, captureHeight, buffersize)
 	
@@ -22,10 +29,14 @@ class ImageServer:
 				time.sleep(0.01) #The list was empty, wait for image to appear
 				continue #Retry to grab a frame
 
-			print("Sending slice " + str(sliceNr))	
 			ret, img = cv2.threshold(img, 8, 255, cv2.THRESH_TOZERO) #Tresh to help image compression (costs no performance)
-			ret, img = cv2.imencode(".jpg", img, [cv2.IMWRITE_JPEG_QUALITY, 95]) 
-		
+			self.compressQueue.append((sliceNr, img)) #Put the image in the compression Queue
+			
+			#Threads compress the image in the background here
+			
+			while len(self.sendQueue) == 0: time.sleep(0.05) #Wait for an image to enter the send queue
+			print("Sending slice " + str(sliceNr))	
+			sliceNr, img = self.sendQueue.pop(0) #Get a compressed image from the queue
 			connection.send(struct.pack(">I", len(img))) #Send the 4 byte length prefix
 			connection.send(struct.pack(">I", sliceNr)) #Send the 4 byte sliceNr
 			connection.sendall(img.tostring()) #Send the actual image
@@ -43,10 +54,15 @@ class ImageServer:
 	def isReady(self):
 		return self.serverReady
 
-	def __init__(self, captureWidth=1280, captureHeight=720, buffersize=1):
+	def __init__(self, captureWidth=1280, captureHeight=720, buffersize=1, compressThreads=3):
 		self.serve = True
 		self.serverReady = False
-		self.clientConnected = False	
+		self.clientConnected = False
+		self.compressQueue = []
+		self.sendQueue = []	
 		serverThread = threading.Thread(target=self.imageServerThread, args=(captureWidth, captureHeight, buffersize))
 		serverThread.start()
+		
+		for i in range(0, compressTheads): threading.Thread(target=self.compressThread).start()
+		
 		print("Image Server started")
