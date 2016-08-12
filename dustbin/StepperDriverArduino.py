48fbd2d1df013d0acdaec323ffeb3db0c1582029
= <=>
2016-08-12 20:11:12 +0200

    changes

--- dustbin/StepperDriverArduino.py
+++ dustbin/StepperDriverArduino.py
@@ -1,50 +1,49 @@
 import glob, sys, time, os
 if not os.getegid() == 0: sys.exit("Run as root for serial port access.")
-from pyA20.gpio import gpio
-from pyA20.gpio import port
+import RPi.GPIO as GPIO
 
 class StepperDriver:
 	def goUp(self):
-		gpio.output(self.serialPin1, 0)
-		gpio.output(self.serialPin2, 1)
+		GPIO.output(self.serialPin1, GPIO.LOW)
+		GPIO.output(self.serialPin2, GPIO.HIGH)
 		self.fireCommand()
 			
 	def goDown(self):
-		gpio.output(self.serialPin1, 0)
-		gpio.output(self.serialPin2, 0)
+		GPIO.output(self.serialPin1, GPIO.LOW)
+		GPIO.output(self.serialPin2, GPIO.LOW)
 		self.fireCommand()
 		
 	def goBottom(self):
-		gpio.output(self.serialPin1, 1)
-		gpio.output(self.serialPin2, 0)
+		GPIO.output(self.serialPin1, GPIO.HIGH)
+		GPIO.output(self.serialPin2, GPIO.LOW)
 		self.fireCommand()
 
 	def goTop(self):
-		gpio.output(self.serialPin1, 1)
-		gpio.output(self.serialPin2, 1)
+		GPIO.output(self.serialPin1, GPIO.HIGH)
+		GPIO.output(self.serialPin2, GPIO.HIGH)
 		self.fireCommand()
 
 	def fireCommand(self):
-		gpio.output(self.enablePin, 1)		
+		GPIO.output(self.enablePin, GPIO.HIGH)
 		time.sleep(0.05) #Wait for the Arduino to read
-		gpio.output(self.enablePin, 0)
+		GPIO.output(self.enablePin, GPIO.LOW)
 		while gpio.input(self.statusPin) == 0: time.sleep(0.1) #Wait for the driver to finish
 		
 	def __init__(self):
-		gpio.init()
+		GPIO.setmode(GPIO.BOARD)
 		
-		self.enablePin = port.PA8 #Pin to enable data transfer to the Arduino
-		self.serialPin1 = port.PA9 #Serial1 of the Arduino
-		self.serialPin2 = port.PA10 #Serial2 of the Arduino
-		self.statusPin = port.PA20 #Status Pin of the Arduino
+		self.enablePin = 8 #Pin to enable data transfer to the Arduino
+		self.serialPin1 = 9 #Serial1 of the Arduino
+		self.serialPin2 = 10 #Serial2 of the Arduino
+		self.statusPin = 20 #Status Pin of the Arduino
 		
 		#Configure the Pins
-		gpio.setcfg(self.enablePin, gpio.OUTPUT)
-		gpio.setcfg(self.serialPin1, gpio.OUTPUT)
-		gpio.setcfg(self.serialPin2, gpio.OUTPUT)
-		gpio.setcfg(self.statusPin, gpio.INPUT)
+		GPIO.setup(self.enablePin, GPIO.IN)
+		GPIO.setup(self.serialPin1, GPIO.OUT)
+		GPIO.setup(self.serialPin2, GPIO.OUT)
+		GPIO.setup(self.statusPin, GPIO.IN)
 		
 		#Set all to zero
-		gpio.output(self.enablePin, 0)
-		gpio.output(self.serialPin1, 0)
-		gpio.output(self.serialPin2, 0)
+		GPIO.output(self.enablePin, GPIO.LOW)
+		GPIO.output(self.serialPin1, GPIO.LOW)
+		GPIO.output(self.serialPin2, GPIO.LOW)
