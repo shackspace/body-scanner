c08b7f84b14ac0ef1088f02fa57e80f7749b4b7b
= <=>
2016-08-19 21:17:43 +0200

    GPIO numbers

--- StepperDriverArduino.py
+++ StepperDriverArduino.py
@@ -32,10 +32,10 @@ class StepperDriver:
 	def __init__(self):
 		GPIO.setmode(GPIO.BOARD)
 		
-		self.enablePin = 6 #Pin to enable data transfer to the Arduino
-		self.serialPin1 = 19 #Serial1 of the Arduino
-		self.serialPin2 = 13 #Serial2 of the Arduino
-		self.statusPin = 26 #Status Pin of the Arduino
+		self.enablePin = 31 #Pin to enable data transfer to the Arduino
+		self.serialPin1 = 35 #Serial1 of the Arduino
+		self.serialPin2 = 33 #Serial2 of the Arduino
+		self.statusPin = 37 #Status Pin of the Arduino
 		
 		#Configure the Pins
 		GPIO.setup(self.enablePin, GPIO.OUT)
